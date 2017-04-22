ff083173670ef7ab2efa61aae9c9e462bfd7b44e
Brigitte Huneke <brigittehuneke@gmail.com>
2017-04-21 19:30:40 -0700

    worked through general layout

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import logo from './assets/logo.svg';
 import './App.css';
 import IconsBar from './components/IconsBar';
+import Description from './components/Description';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div>
-          <img src={logo} alt="logo" />
+        <div className='main-content'>
+          <div >
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <h4> S U B   H E A D I N G   T A G L I N E </h4>
+          <Description />
+          <IconsBar />
         </div>
-        <IconsBar/>
+        <div className='picture'/> 
       </div>
     );
   }
--- src/components/IconsBar.js
+++ src/components/IconsBar.js
@@ -3,14 +3,15 @@ import email from '../assets/email.svg';
 import github from '../assets/github.svg';
 import instagram from '../assets/instagram.svg';
 import linkedin from '../assets/linkedin.svg';
+import '../App.css';
 
 export default function IconsBar(props)  {
   return (
     <div>
-      <img src={email} alt='email'/>
-      <img src={github} alt='github'/>
-      <img src={instagram} alt='instagram'/>
-      <img src={linkedin} alt='linkedin'/>
+      <img src={email} className='icon' alt='email'/>
+      <img src={github} className='icon' alt='github'/>
+      <img src={instagram} className='icon' alt='instagram'/>
+      <img src={linkedin} className='icon' alt='linkedin'/>
     </div>
   );
 }
